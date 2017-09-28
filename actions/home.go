6eaf4caf96084ab28c78240bf7522b8e21d18d53
ZhiFeng Hu <huzhifeng@douyu.tv>
2017-09-28 11:17:45 +0800

    Add goth login

--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,22 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"github.com/gobuffalo/buffalo"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+
+	m := make([]map[string]string, 0)
+
+	m = append(m, map[string]string{"amazon": "Amazon"})
+	m = append(m, map[string]string{"bitbucket": "Bitbucket"})
+	m = append(m, map[string]string{"facebook": "facebook"})
+	m = append(m, map[string]string{"github": "github"})
+	m = append(m, map[string]string{"gitlab": "gitlab"})
+	m = append(m, map[string]string{"twitter": "twitter"})
+	m = append(m, map[string]string{"google": "google"})
+	c.Set("Providers", m)
 	return c.Render(200, r.HTML("index.html"))
 }
