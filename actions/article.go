ebd7c9619f8f02bf63a4e4aed0ced4ba28e4f0ea
ZhiFeng Hu <huzhifeng@douyu.tv>
2017-11-20 10:42:08 +0800

    Update edit

--- actions/article.go
+++ actions/article.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gobuffalo/buffalo"
 	"github.com/markbates/pop"
 	"github.com/pkg/errors"
+	"golang.org/x/tools/go/gcimporter15/testdata"
 )
 
 // ArticleCreate default implementation.
@@ -115,18 +116,29 @@ func ArticleEdit(c buffalo.Context) error {
 	m = append(m, map[string]string{"gplus": "google"})
 	c.Set("Providers", m)
 
+	username := s.Get("username")
+	if username == nil {
+		c.Set("message", "请先登录")
+		return c.Render(422, r.HTML("message.html"))
+	}
+
+	tid := c.Param("tid")
+	if tid == "" {
+		c.Set("message", "tid can not be empty")
+		return c.Render(422, r.HTML("message.html"))
+	}
 	// Get the DB connection from the context
 	tx := c.Value("tx").(*pop.Connection)
-	articles := &models.Articles{}
+	article := &models.Article{}
 	// You can order your list here. Just change
-	err := tx.Order("created_at desc").All(articles)
+	err := tx.Find(&article, tid)
 	// to:
 	// err := tx.Order("create_at desc").All(articles)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	// Make articles available inside the html template
-	c.Set("articles", articles)
+	c.Set("article", article)
 
 	return c.Render(200, r.HTML("article/edit.html"))
 }
@@ -141,18 +153,41 @@ func ArticleSaveEdit(c buffalo.Context) error {
 	m = append(m, map[string]string{"gplus": "google"})
 	c.Set("Providers", m)
 
+	username := s.Get("username")
+	if username == nil {
+		c.Set("message", "请先登录")
+		return c.Render(422, r.HTML("message.html"))
+	}
+
+	tid := c.Param("tid")
+	if tid == "" {
+		c.Set("message", "tid can not be empty")
+		return c.Render(422, r.HTML("message.html"))
+	}
 	// Get the DB connection from the context
 	tx := c.Value("tx").(*pop.Connection)
-	articles := &models.Articles{}
+	article := &models.Article{}
 	// You can order your list here. Just change
-	err := tx.Order("created_at desc").All(articles)
+	err := tx.Find(&article, tid)
 	// to:
 	// err := tx.Order("create_at desc").All(articles)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	// Make articles available inside the html template
-	c.Set("articles", articles)
+	c.Request().ParseForm()
+	article.Title = c.Request().Form.Get("title")
+	article.Content = c.Request().Form.Get("content")
+
+	// Validate the data from the html form
+	verrs, err := tx.ValidateAndUpdate(article)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	if verrs.HasAny() {
+		c.Set("errors", verrs)
+		return c.Render(422, r.HTML("message.html"))
+	}
+	c.Set("message", "Update Success")
 
 	return c.Render(200, r.JSON("message"))
 }
